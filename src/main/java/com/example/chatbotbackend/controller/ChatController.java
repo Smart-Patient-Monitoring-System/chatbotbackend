0d91a628351e71cd28662664b9b8c459a4c7f993
Anupa200212 <supulanupa@gmail.com>
2026-02-18 11:52:57 +0530

    Add ChatController to handle chat interactions via a POST endpoint.

--- src/main/java/com/example/chatbotbackend/controller/ChatController.java
+++ src/main/java/com/example/chatbotbackend/controller/ChatController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/api/chat/chat")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class ChatController {
