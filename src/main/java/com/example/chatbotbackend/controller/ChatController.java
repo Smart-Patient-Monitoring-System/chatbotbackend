a082091e2fea82ee104074929f7279792e13ee37
Anupa200212 <supulanupa@gmail.com>
2026-02-20 00:33:06 +0530

    implement basic chat API endpoint and configure application properties.

--- src/main/java/com/example/chatbotbackend/controller/ChatController.java
+++ src/main/java/com/example/chatbotbackend/controller/ChatController.java
@@ -6,18 +6,15 @@ import com.example.chatbotbackend.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/api/chat/chat")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class ChatController {
-    private final ChatService chatService   ;
+    private final ChatService chatService;
 
     @PostMapping
     public ChatResponse chat(@RequestBody ChatRequest request) {
         return new ChatResponse(
-                chatService.getReply(request.getMessage())
-        );
+                chatService.getReply(request.getMessage()));
     }
 }
